1514f439ebbb0fcb8401247650789f4de88da14d
svinchy <svinchy@gmail.com>
2022-04-06 00:28:38 +0400

    add responsive

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -5,7 +5,7 @@ import { Logo, Logo2 } from '../../components'
 import SOLOLAKI_JPG from '../../assets/images/CoverPage/sololaki.jpg'
 import RUSTAVELI_JPG from '../../assets/images/CoverPage/rustaveli.jpg'
 
-import style, { styleLink, styleLogo } from './style'
+import style, { styleLink, styleLogo, styleHeading, styleUnderConstruction } from './style'
 
 const logo = {
   proto: Logo,
@@ -33,15 +33,42 @@ const link = {
   proto: [Link, Box],
   class: [styleLink]
 }
+const Heading = {
+  tag: 'h4',
+  proto: [Text, Box],
+  class: [styleHeading],
+  props: {
+    color: 'cream .65'
+  }
+}
 
 const Sololaki = {
   proto: link,
+  props: { href: '/Sololaki' },
   style: {
     backgroundImage: 'linear-gradient(rgba(0, 0, 0, .25), rgba(0, 0, 0, .85)),url(' + SOLOLAKI_JPG + ')',
     '@media only screen and (min-width: 1024px)': {
+      cursor: 'pointer',
+      alignItems: 'flex-end',
+      justifyContent: 'flex-end',
       '&:hover': {
         backgroundPosition: 'top left -100px',
-        backgroundImage: 'linear-gradient(rgba(0, 0, 0, .15), rgba(0, 0, 0, .35)),url(' + SOLOLAKI_JPG + ')'
+        backgroundImage: 'linear-gradient(rgba(0, 0, 0, .15), rgba(0, 0, 0, .45)),url(' + SOLOLAKI_JPG + ')'
+      }
+    }
+  },
+
+  heading: {
+    proto: Heading,
+    props: {
+      text: 'sololaki'
+    },
+
+    style: {
+      '@media only screen and (min-width: 1024px)': {
+        borderTopLeftRadius: `${40 / 16}em`,
+        borderBottomLeftRadius: `${40 / 16}em`,
+        padding: `${20 / 16}em ${10 / 16}em ${20 / 16}em ${20 / 16}em`
       }
     }
   }
@@ -52,9 +79,42 @@ const Rustaveli = {
   style: {
     backgroundImage: 'linear-gradient(rgba(0, 0, 0, .25), rgba(0, 0, 0, .85)), url(' + RUSTAVELI_JPG + ')',
     '@media only screen and (min-width: 1024px)': {
+      alignItems: 'flex-end',
+      justifyContent: 'flex-start',
       '&:hover': {
         backgroundPosition: 'top left 100px',
-        backgroundImage: 'linear-gradient(rgba(0, 0, 0, .15), rgba(0, 0, 0, .35)), url(' + RUSTAVELI_JPG + ')'
+        backgroundImage: 'linear-gradient(rgba(0, 0, 0, .15), rgba(0, 0, 0, .45)), url(' + RUSTAVELI_JPG + ')'
+      }
+    }
+  },
+
+  heading: {
+    proto: Heading,
+    props: {
+      text: 'rustaveli',
+      position: 'relative'
+    },
+    style: {
+      '@media only screen and (min-width: 1024px)': {
+        borderTopRightRadius: `${40 / 16}em`,
+        borderBottomRightRadius: `${40 / 16}em`,
+        padding: `${20 / 16}em ${20 / 16}em ${20 / 16}em ${10 / 16}em`
+      },
+      '@media only screen and (max-width: 1024px)': {
+        display: 'flex',
+        flexFlow: 'column-reverse',
+        alignItems: 'center',
+        gap: `${10 / 30}em`
+
+      }
+    },
+    underConstruction: {
+      tag: 'span',
+      proto: [Text, Box],
+      class: [styleUnderConstruction],
+      props: {
+        text: 'under construction',
+        color: 'orange'
       }
     }
   }
--- src/pages/Landing/style.js
+++ src/pages/Landing/style.js
@@ -1,15 +1,28 @@
 'use strict'
 
 export const styleLink = {
-  border: '10px solid green',
   flex: 1,
   backgroundAttachment: 'fixed',
   backgroundSize: 'cover',
+  boxSizing: 'border-box',
+  display: 'flex',
+  textDecoration: 'none',
   '@media only screen and (min-width: 1024px)': {
     transition: 'flex .8s ease-in-out, background-position .8s ease-in-out, background-image .8s ease-in-out',
-    '&:hover': {
-      flex: 8
-    }
+    paddingBottom: `${100 / 16}em`,
+    '&:hover': { flex: 8 },
+    '&:hover > h4': {
+      backdropFilter: 'blur(10px)',
+      letterSpacing: '2.5px',
+      color: 'rgba(244, 233, 217, .85)'
+    },
+    '&:hover > h4 > span': { opacity: 1 },
+    '&:not(:hover) > h4 > span': { transition: 'opacity .2s ease-in-out' }
+  },
+
+  '@media only screen and (max-width: 1024px)': {
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 }
 
@@ -17,10 +30,39 @@ export const styleLogo = {
   transform: 'translate(-50%, -50%) scale(1.1)',
   pointerEvents: 'none',
   opacity: 0
+}
 
+export const styleHeading = {
+  textTransform: 'uppercase',
+  letterSpacing: '1.5px',
+  fontWeight: 700,
+  '@media only screen and (min-width: 1024px)': {
+    transition: 'backdrop-filter .5s ease-in-out, letter-spacing .8s ease-in-out'
+  },
+  '@media only screen and (max-width: 1024px)': {
+    fontSize: `${30 / 16}em`
+
+  }
+}
+
+export const styleUnderConstruction = {
+  textTransform: 'initial',
+  fontWeight: 500,
+  whiteSpace: 'nowrap',
+  fontSize: `${15 / 16}em`,
+  '@media only screen and (min-width: 1024px)': {
+    transition: 'opacity 1s ease-in-out',
+    opacity: 0,
+    position: 'absolute',
+    bottom: '3px',
+    left: '12px'
+  }
 }
 export default {
-  border: '10px solid red',
   flex: 1,
-  display: 'flex'
+  display: 'flex',
+  '@media only screen and (max-width: 1024px)': {
+    flexFlow: 'column'
+
+  }
 }
