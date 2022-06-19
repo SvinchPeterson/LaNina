73b264592dae0d8f6f1a21559b4bf3b6d9046d65
svinchy <svinchy@gmail.com>
2022-06-19 22:38:33 +0400

    clean code on wine article

--- src/articles/Wine/index.js
+++ src/articles/Wine/index.js
@@ -5,35 +5,28 @@ import WINE_JPG from '../../assets/images/sololaki/wine.jpg'
 
 const image = {
   proto: Box,
+
   props: {
-    boxSize: `${450 / 16}em ${600 / 16}em`,
+    boxSize: `H H2`,
     position: 'absolute',
+    margin: '0 G 0 0',
     '@tabletL': {
-      boxSize: `80% ${600 / 16}em`
+      width: '80%',
+      margin: '0'
     },
-    '@mobileS': {
-      boxSize: `100% ${400 / 16}em`
-    }
+    '@mobileS': { boxSize: '100% G3' }
   },
   style: {
     backgroundImage: 'linear-gradient(rgba(60,84, 72, .2), rgba(60,84, 72, .35)),url(' + WINE_JPG + ')',
-    backgroundSize: 'cover',
+    backgroundSize: 'contain',
+    backgroundPosition: 'left 300px bottom 150px',
     backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed',
     zIndex: 2,
-    backgroundPosition: 'center center',
-    '@media only screen and (min-width: 1366px)': {
-      backgroundAttachment: 'fixed',
-      backgroundPosition: 'left 300px bottom 150px',
-      marginRight: `${300 / 16}em`,
-      backgroundSize: 'contain'
-    },
-    '&:before': {
-      content: '""',
-      position: 'absolute',
-      bottom: '100%',
-      left: 0,
-      width: '600px',
-      height: '3px'
+    '@media only screen and (max-width: 1366px)': {
+      backgroundPosition: 'center center',
+      backgroundSize: 'cover',
+      backgroundAttachment: 'initial'
     }
   }
 }
@@ -44,12 +37,14 @@ const paragraph = {
     height: `${800 / 16}em`,
     position: 'absolute',
     padding: '0 D 0 E',
+    margin: '0 0 0 H1',
     flexAlign: 'center flex-end',
     '@tabletL': {
       height: 'fit-content',
       padding: '0 D',
       background: 'cream2',
-      top: '100%'
+      top: '100%',
+      margin: '0'
     },
     '@mobileS': {
       padding: '0 B2',
@@ -57,37 +52,25 @@ const paragraph = {
     }
   },
   style: {
-    // border: '3px solid green',
-    // borderLeft: '3px solid rgba(168, 98, 63, 1)',
     borderTop: 'none',
     '@media only screen and (min-width: 1366px)': {
       background: 'linear-gradient(rgba(248, 241, 227, 0), rgba(248, 241, 227, 0),rgba(42, 81, 61, .35))',
       backgroundAttachment: 'fixed',
-      backgroundPosition: 'bottom 200px center',
-      marginLeft: '500px'
+      backgroundPosition: 'bottom 200px center'
     },
     '@media only screen and (max-width: 1366px)': {
       zIndex: 4,
       height: 'fit-content'
     }
   },
+
   span: {
     proto: [wineText, Box],
     props: {
-      fontSize: `${14 / 16}em`,
-      maxWidth: `${300 / 14}em`,
-      display: 'block',
+      fontSize: 'Z',
+      maxWidth: 'G1',
       color: 'black .35',
-      '@tabletL': {
-        maxWidth: `${620 / 14}em`
-      }
-    },
-    style: {
-      '@media only screen and (max-width: 1366px)': {
-        textAlign: 'left'
-      }
-      // border: '3px solid red',
-      // backdropFilter: 'blur(3px)'
+      '@tabletL': { maxWidth: 'H3' }
     }
   }
 }
@@ -104,6 +87,7 @@ export default {
     '@mobileS': { margin: 'D 0 F 0' }
   },
   attr: { id: 'wine' },
+
   image,
   paragraph
 }
