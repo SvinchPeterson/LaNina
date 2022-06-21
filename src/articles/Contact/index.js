20d3e68e5b60e4af625e8357d531ec476580207d
svinchy <svinchy@gmail.com>
2022-06-21 13:00:05 +0400

    add contacts

--- src/articles/Contact/index.js
+++ src/articles/Contact/index.js
@@ -19,8 +19,10 @@ export const Contact = {
     }
   },
   ...[
-    { props: { text: 'svinchy@gmail.com' } },
-    { props: { text: '+995 571 017 170' } }
+    { props: { text: '+995 322 502222' } },
+    { props: { text: '+995593 345158' } },
+    { props: { text: 'info@bbhospitality.ge' } },
+    { props: { text: 'reservations@bbhospitality.ge' } }
   ]
 
 }
