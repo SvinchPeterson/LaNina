ebee0cd33c98470cb70ee25843944a254df2809d
svinchy <svinchy@gmail.com>
2022-07-06 17:16:32 +0400

    clean code

--- src/components/RoomTabs/index.js
+++ src/components/RoomTabs/index.js
@@ -25,21 +25,12 @@ const tabParagraph = {
   props: {
     color: 'cream2',
     fontSize: 'Z',
-    padding: `0 Z1 0 Z1`,
-    '@tabletL': {
-      // fontSize: 'A',
-      maxWidth: 'H'
-    },
-    '@mobileL': { padding: 'C1 B1' },
-    '@mobileS': { padding: 'C1' }
+    padding: `0 Z1 0 Z1`
   },
   style: {
-    // maxWidth: '700px',
-
-    // border: '4px solid red',
     zIndex: 10,
     opacity: 0,
-    maxWidth: '330px',
+    maxWidth: `${330 / 14}em`,
     textAlign: 'justify',
     boxSizing: 'border-box',
     pointerEvents: 'none',
@@ -49,7 +40,6 @@ const tabParagraph = {
     letterSpacing: '.5px',
     wordSpacing: '-1.5px',
     fontWeight: 400
-    // border: '2px solid red'
   },
   class: {
     show: (element, state) => state.activeTab === element.parent.parent.key ? { opacity: 1 } : {}
@@ -63,17 +53,7 @@ export const RoomTab = {
     height: 'G1',
     padding: 'B',
     position: 'relative',
-    round: 'X',
-    '@tabletL': {
-      minWidth: '100%',
-      height: 'H'
-    },
-    '@mobileL': {
-      minWidth: `100%`,
-      minHeight: '100%',
-      height: 'G3'
-    }
-    // '@mobileM': { height: 'G' }
+    round: 'X'
   },
 
   style: {
@@ -87,14 +67,6 @@ export const RoomTab = {
     textDecoration: 'none',
     position: 'relative',
     backdropFilter: 'blur(2px)',
-    // filter: 'grayscale(100%)',
-
-    // boxShadow: '0px 0px 2px 0px rgba(0, 0, 0, .35)',
-    // overflow: 'hidden',
-    // '&:hover:after': {
-    //   color: '#FFCEA8',
-    //   opacity: '.85'
-    // },
     '&:before': {
       content: '""',
       position: 'absolute',
@@ -112,8 +84,8 @@ export const RoomTab = {
       fontSize: `${10 / 14}em`,
       fontWeight: 700,
       position: 'absolute',
-      width: '125px',
-      padding: '12px 0',
+      width: `${125 / 16}em`,
+      padding: `${12 / 16}em 0`,
       top: `${0 / 10}em`,
       left: `${0 / 10}em`,
       backdropFilter: 'blur(10px)',
@@ -123,17 +95,6 @@ export const RoomTab = {
       color: 'rgba(248, 241, 227, 1)',
       textTransform: 'uppercase',
       whiteSpace: 'nowrap'
-    },
-
-    '@media only screen and (min-width: 1225px)': {
-      '&:hover': { flex: 5.5, backdropFilter: 'blur(10px)' },
-      '&:hover > div > p': { opacity: 1 },
-      '&:not(:hover) > div > p': {
-        transitionDelay: '0s',
-        transition: 'opacity 0s ease-in-out'
-      },
-      '&:hover:before': { opacity: 1 },
-      '&:hover img': { opacity: 1 }
     }
   },
   on: {
