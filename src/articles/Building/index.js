846ace108ddebf86f7d9ad1f9b61c9cb566feb09
svinchy <svinchy@gmail.com>
2022-07-06 17:05:18 +0400

    delete responsive code prom building article

--- src/articles/Building/index.js
+++ src/articles/Building/index.js
@@ -10,22 +10,14 @@ const buildingHistory = {
     maxWidth: `${650 / 14.3}em`,
     color: 'black .75',
     padding: '0 D1',
-    // fontWeight: '500',
-    fontFamily: 'Dejavu',
-    '@tabletL': { maxWidth: `${650 / 15}em` },
-    '@tabletS': { padding: '0 C1' },
-    '@mobileM': { padding: '0 B2' },
-
-    '@mobileXS': { padding: '0 B1' }
+    fontFamily: 'Dejavu'
   },
   style: {
-    // border: '2px solid red',
     textAlign: 'justify',
     wordSpacing: '-1px',
     fontSize: `${14.3 / 16}em`,
     lineHeight: `${21 / 14.3}em`,
-    '@media only screen and (max-width: 480px)': { fontSize: `${14 / 16}em` },
-    '> div': { lineHeight: '25px', letterSpacing: '.8px' }
+    '> div': { lineHeight: `${25 / 14.3}em`, letterSpacing: `${0.8 / 14.3}em` }
   }
 }
 
@@ -35,7 +27,6 @@ export default {
     minHeight: '100%',
     height: '100%',
     flexAlign: 'center center'
-    // padding: 'G2 0 E2 0'
   },
   attr: { id: 'residence' },
 
--- src/style.js
+++ src/style.js
@@ -18,6 +18,5 @@ export default {
   p: { margin: 0 },
   h6: { margin: 0 },
   // border: '10px solid red',
-  background: 'radial-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 1))',
-  '@media only screen and (min-width: 1680px)': { fontSize: '19px' }
+  background: 'radial-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 1))'
 }
