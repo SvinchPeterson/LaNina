f701ac7048bb938a2b27eda0ca91c549aae849dd
svinchy <svinchy@gmail.com>
2022-07-06 17:01:09 +0400

    clean banner file from responsive codes

--- src/pages/Sololaki/banner.js
+++ src/pages/Sololaki/banner.js
@@ -12,21 +12,7 @@ const heading = {
     top: '45%',
     right: '17.5%',
     flexFlow: 'column',
-    flexAlign: 'flex-start flex-start',
-    '@tabletL': {
-      right: '5.5%'
-    },
-    '@mobileM': {
-      top: '50%',
-      right: 'auto',
-      left: '50%'
-    }
-  },
-  style: {
-    '@media only screen and (max-width: 560px)': {
-      right: 'initial',
-      transform: 'translate(-50%, -50%)'
-    }
+    flexAlign: 'flex-start flex-start'
   },
 
   span: {
@@ -37,22 +23,7 @@ const heading = {
       color: 'orange2 .55',
       position: 'absolute',
       top: '0',
-      right: 'A',
-      '@tabletL': {
-        top: '0',
-        right: '0'
-      },
-      '@mobileL': {
-        top: 'auto',
-        right: 'C',
-        color: 'orange2',
-        bottom: '-B1'
-      },
-      '@mobileM': {
-        fontSize: 'B',
-        top: 'A2',
-        right: 'B1'
-      }
+      right: 'A'
     },
     style: {
       width: 'fit-content',
@@ -62,15 +33,7 @@ const heading = {
       letterSpacing: `${10 / 16}em`,
       animationName: letterSpacing,
       animationDuration: '2.5s',
-      animationTimingFunction: 'ease-in-out',
-      '@media only screen and (max-width: 768px)': {
-        letterSpacing: `${8 / 16}em`,
-        animationName: letterSpacing2
-      },
-      '@media only screen and (max-width: 560px)': {
-        letterSpacing: `${5 / 16}em`,
-        animationName: letterSpacing4
-      }
+      animationTimingFunction: 'ease-in-out'
     }
   },
 
@@ -79,11 +42,7 @@ const heading = {
     props: {
       text: 'BB Residence',
       color: 'gold .75',
-      margin: '0',
-      '@mobileM': {
-        maxWidth: 'D1',
-        lineHeight: `${70 / 70}em`
-      }
+      margin: '0'
     },
     style: {
       fontSize: `${100 / 16}em`,
@@ -94,21 +53,7 @@ const heading = {
       animationTimingFunction: 'ease-in-out',
       fontWeight: 100,
       letterSpacing: `${-3 / 120}em`,
-      textShadow: '0px 0px 20px rgba(0, 0, 0, .35)',
-      '@media only screen and (max-width: 768px)': {
-        fontSize: `${80 / 16}em`,
-        fontWeight: 100,
-        animationDuration: '1.5s',
-        animationName: height2,
-        height: `${100 / 80}em`
-      },
-      '@media only screen and (max-width: 560px)': {
-        fontWeight: '100',
-        fontSize: `${70 / 16}em`,
-        animationName: height3,
-        height: `${170 / 70}em`,
-        animationDuration: '3s'
-      }
+      textShadow: '0px 0px 20px rgba(0, 0, 0, .35)'
     }
   }
 }
@@ -119,15 +64,7 @@ const image = {
     boxSize: '60% 100%',
     position: 'absolute',
     top: '0',
-    left: '0',
-    '@tabletL': {
-      boxSize: '80% 60%',
-      top: '48%',
-      left: '0%'
-    },
-    '@mobileM': {
-      boxSize: '100% 105%'
-    }
+    left: '0'
   },
   style: {
 
@@ -138,29 +75,15 @@ const image = {
     animationDuration: '7s',
     // animationTimingFunction: 'ease-in-out',
     animationTimingFunction: 'cubic-bezier(.17,.61,.3,.71)',
-    '@media only screen and (min-width: 1366px)': {
-      backgroundImage: 'url(' + SOLOLAKI_JPG + ')',
-      backgroundAttachment: 'fixed'
-    },
-    '@media only screen and (max-width: 1366px)': {
-      backgroundImage: 'url(' + SOLOLAKI_TABLET_JPG + ')',
-      transform: 'translate(0%, -50%)'
-    },
-    '@media only screen and (max-width: 560px)': {
-      backgroundPosition: 'bottom left',
-      animationName: position2
-
-    },
+    backgroundImage: 'url(' + SOLOLAKI_JPG + ')',
+    backgroundAttachment: 'fixed',
     '&:before': {
       content: '""',
       width: '100%',
       height: '100%',
       display: 'block',
       background: 'radial-gradient(rgba(60,84, 72, .45), rgba(60,84, 72, .8))',
-      animationTimingFunction: 'cubic-bezier(.17,.61,.3,.71)',
-      '@media only screen and (max-width: 560px)': {
-        background: 'linear-gradient(rgba(60,84, 72, .65), rgba(60,84, 72, .85))'
-      }
+      animationTimingFunction: 'cubic-bezier(.17,.61,.3,.71)'
     }
   }
 }
@@ -174,9 +97,6 @@ const container = {
   style: {
     overflow: 'hidden',
     background: 'radial-gradient(rgba(60,84, 72, .8), rgba(60,84, 72, 1))',
-    '@media only screen and (max-width: 1366px)': {
-      background: 'linear-gradient( rgba(60,84, 72, .95), rgba(60,84, 72, .85))'
-    },
     '&:after': {
       content: '""',
       width: '100%',
