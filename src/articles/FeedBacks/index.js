ec3ff70a9d8c109c09a9226dcf8873e83b1cc63c
svinchy <svinchy@gmail.com>
2022-07-11 02:46:42 +0400

    add scroll on feedbacks

--- src/articles/FeedBacks/index.js
+++ src/articles/FeedBacks/index.js
@@ -2,25 +2,30 @@
 
 import { Box, Text, Img } from '@symbo.ls/symbols'
 
-import RIGHT_ARROW_PNG from '../../assets/icons/arrow-right.png'
+// import RIGHT_ARROW_PNG from '../../assets/icons/arrow-right.png'
+import LEFT_ARROW_PNG from '../../assets/icons/leftBlack.png'
+import RIGHT_ARROW_PNG from '../../assets/icons/rightBlack.png'
 
 const feedBackParagraph = {
   proto: Box,
   props: {
-    padding: 'B2 B2',
+    padding: 'C2 D C D',
     flexFlow: 'column',
     flexAlign: 'flex-start flex-start',
     background: 'black .05',
     maxHeight: 'G',
-    minWidth: 'H2',
+    minWidth: '100%',
+    maxWidth: '100%',
     position: 'relative',
     round: 'Y2'
   },
   style: {
+    boxSizing: 'border-box',
     background: 'linear-gradient(rgba(0, 0, 0, .03), rgba(0, 0, 0, .08))',
     height: '500px',
     maxWidth: 'fit-content',
     border: '.1px solid rgba(0, 0, 0, .01)'
+    // border: '5px solid red'
   },
   name: {
     tag: 'caption',
@@ -64,8 +69,8 @@ const feedBackParagraph = {
       // margin: '0 0 0 auto',
       padding: '0 Y2 0 0',
       position: 'absolute',
-      right: 'D',
-      bottom: 'B2',
+      right: 'D2',
+      bottom: 'C',
       color: 'black .55'
     },
     style: {
@@ -75,73 +80,117 @@ const feedBackParagraph = {
       fontSize: `${14 / 16}em`
     }
   }
+}
+
+const navArrows = {
+  proto: Box,
+  props: {
+    flexAlign: 'center flex-start',
+    gap: 'B'
+  },
+  childProto: {
+    proto: Box,
+    tag: 'button',
+    props: {
+      // boxSize: ' A',
+      boxSize: 'B1 B1'
+    },
+    style: {
+      cursor: 'pointer',
+      background: 'none',
+      outline: 'none',
+      border: 'none'
+    },
+    arrow: {
+      proto: [Img, Box],
+      props: {
+        boxSize: '100%',
+        padding: 'W'
+      }
+    }
+  },
+
+  ...[
+    {
+      on: {
+        click: (event, element, state) => {
+          const { content } = element.parent.parent.parent
+          content.node.scrollBy({
+            top: 0,
+            left: -content.node.clientWidth - 16.5 * 1.618
+          })
+        }
+      },
+
+      arrow: { props: { src: LEFT_ARROW_PNG } }
+    },
+
+    {
+      on: {
+        click: (event, element, state) => {
+          const { content } = element.parent.parent.parent
+          content.node.scrollBy({
+            top: 0,
+            left: content.node.clientWidth + 16.5 * 1.618
+          })
+        }
+      },
+
+      arrow: { props: { src: RIGHT_ARROW_PNG } }
+    }
+  ]
 
 }
 
 const heading = {
   proto: Box,
   props: {
-    padding: '0 A A A',
+    padding: 'A A 0 A',
     flexAlign: 'center space-between'
   },
   title: {
     proto: Box,
     tag: 'caption',
-    text: 'feedback',
+    text: 'feedbacks',
     props: {
       fontWeight: '700'
     },
     style: { whiteSpace: 'nowrap', letterSpacing: '.3px' }
   },
-  scroll: {
-    proto: Box,
-    text: 'scroll',
-    props: {
-      fontSize: 'Z',
-      flexAlign: 'center flex-start',
-      gap: 'Z',
-      fontWeight: '700'
-    },
-    style: { letterSpacing: '.7px', textTransform: 'uppercase' }
-    // arrow: {
-    //   proto: [Img, Box],
-    //   props: {
-    //     src: RIGHT_ARROW_PNG,
-    //     boxSize: ' Z2'
-    //   }
-    // }
-  }
+  arrows: { proto: navArrows }
 }
+
 export const feedBacks = {
   proto: Box,
   props: {
     minHeight: 'H',
-
-    flexFlow: 'column',
-    flexAlign: 'flex-start center',
+    overflowX: 'hidden',
+    // flexFlow: 'column',
+    // flexAlign: 'flex-start flex-start',
     minWidth: '500px',
-    maxWidth: '500px',
-    borderTop: '1px solid rgba(0, 0, 0, .75)'
+    maxWidth: '600px'
+    // borderTop: '1px solid rgba(0, 0, 0, .75)'
+  },
 
-  },
   style: {
-    overflow: 'hidden',
+    overflowX: 'hidden',
     margin: '0 auto',
-    marginTop: `${200 / 16}em`,
-    marginBottom: `${200 / 16}em`
+    marginTop: `${300 / 16}em`,
+    marginBottom: `${300 / 16}em`
+    // border: '5px solid red'
   },
-  heading,
+
+  heading: { proto: heading },
   content: {
     proto: Box,
     props: {
       flexAlign: 'flex-start flex-start',
-      gap: 'B2',
-      padding: 'D 0 0 0'
+      gap: 'B',
+      padding: 'A1 0 0 0'
     },
     style: {
-      overflowY: 'auto',
       scrollBehavior: 'smooth',
-      borderTop: '1px solid rgba(0, 0, 0, .75)',
+      // borderTop: '1px solid rgba(0, 0, 0, .75)',
       overflowX: 'auto',
       // border: '4px solid orange',
       // minWidth: '100%',
