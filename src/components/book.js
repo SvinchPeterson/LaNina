f5339eda11041065ab0d6ea8725ce1886043765d
svinchy <svinchy@gmail.com>
2022-07-26 11:41:35 +0400

    ewfef

--- src/components/book.js
+++ src/components/book.js
@@ -9,7 +9,7 @@ export const Book = {
     target: '_blank',
     css: {
       borderTopLeftRadius: '8px',
-      borderLeft: '1.5px solid rgba(245, 245, 245, .75)',
+      borderLeft: '1.5px solid rgba(245, 245, 245, .749)',
       borderTop: '1.5px solid rgba(245, 245, 245, .75)',
       textDecoration: 'none',
       zIndex: 100,
