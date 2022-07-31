72b50acfbf48339bebda563c65038b5fc90affa1
svinchy <svinchy@gmail.com>
2022-08-01 01:15:32 +0400

    fixed book border

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,15 +3,15 @@ import { Link } from 'smbls'
 export const Book = {
   proto: Link,
   props: {
-    padding: 'Y2 W2 0 B',
+    padding: 'Z B1',
+    round: 'B',
+    // padding: 'Y2 W2 0 B',
     color: 'white',
     href: 'https://www.airbnb.com/users/show/404104381',
     target: '_blank',
 
     css: {
-      borderTopLeftRadius: '8px',
-      borderLeft: '1.5px solid rgba(245, 245, 245, .749)',
-      borderTop: '1.5px solid rgba(245, 245, 245, .75)',
+      border: '1px solid rgba(245, 245, 245, .5)',
       textDecoration: 'none',
       zIndex: 100,
       // fontWeight: '900',
