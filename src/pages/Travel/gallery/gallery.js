40c3052eb06caa9180fcdfd905a9d57aa423105c
svinchy <svinchy@gmail.com>
2022-10-17 07:44:14 +0400

    add images

--- src/pages/Travel/gallery/gallery.js
+++ src/pages/Travel/gallery/gallery.js
@@ -1,19 +1,35 @@
 'use strict'
 import { Grid } from 'smbls'
 
-import METEKHI_JPG from '../../../assets/images/travel/snow.jpg'
-import KAZBEGI_JPG from '../../../assets/images/travel/varzia.jpg'
-import MESTIA_JPG from '../../../assets/images/travel/mtsketa.jpg'
+import SNOW_JPG from '../../../assets/images/travel/snow.jpg'
+import SNOW_TABLET_JPG from '../../../assets/images/travel/snow-tablet.jpg'
+import SNOW_MOBILE_JPG from '../../../assets/images/travel/snow-mobile.jpg'
+
+import VARZIA_JPG from '../../../assets/images/travel/varzia.jpg'
+import VARZIA_MOBILE_JPG from '../../../assets/images/travel/varzia-mobile.jpg'
+
+import MTSKETA_JPG from '../../../assets/images/travel/mtsketa.jpg'
+import MTSKETA_TABLET_JPG from '../../../assets/images/travel/mtsketa-tablet.jpg'
+import MTSKETA_MOBILE_JPG from '../../../assets/images/travel/mtsketa-mobile.jpg'
+
 import DANCE_JPG from '../../../assets/images/travel/dance.jpg'
+import DANCE_TABLET_JPG from '../../../assets/images/travel/dance-tablet.jpg'
+import DANCE_MOBILE_JPG from '../../../assets/images/travel/dance-mobile.jpg'
+
 import MUSEUM_JPG from '../../../assets/images/travel/museum.jpg'
+
 import STALIN_JPG from '../../../assets/images/travel/stalin.jpeg'
+import STALIN_TABLET_JPG from '../../../assets/images/travel/stalin-tablet.jpeg'
+import STALIN_MOBILE_JPG from '../../../assets/images/travel/stalin-mobile.jpeg'
 
 const gridGallery = {
   extend: Grid,
   ...[
     {
       props: {
-        backgroundImage: 'url(' + METEKHI_JPG + ')',
+        backgroundImage: 'url(' + SNOW_JPG + ')',
+        '@tabletS': { backgroundImage: 'url(' + SNOW_TABLET_JPG + ')' },
+        '@mobileM': { backgroundImage: 'url(' + SNOW_MOBILE_JPG + ')' },
         gridColumn: '1 / span 2',
         gridRow: '1 / span 2'
       }
@@ -22,41 +38,45 @@ const gridGallery = {
     {
       props: {
         gridColumn: '3 / span 4',
-        backgroundImage: 'url(' + KAZBEGI_JPG + ')',
-        '@tabletS': {
-          gridRow: '2'
-        }
+        backgroundImage: 'url(' + VARZIA_JPG + ')',
+        '@tabletS': { backgroundImage: 'url(' + VARZIA_MOBILE_JPG + ')' }
       }
     },
 
     {
       props: {
         gridColumn: '3 / span 4',
-        backgroundImage: 'url(' + MESTIA_JPG + ')'
+        backgroundImage: 'url(' + MTSKETA_JPG + ')',
+        '@tabletS': { backgroundImage: 'url(' + MTSKETA_TABLET_JPG + ')' },
+        '@mobileM': { backgroundImage: 'url(' + MTSKETA_MOBILE_JPG + ')' }
       }
     },
 
     {
       props: {
         backgroundImage: 'url(' + DANCE_JPG + ')',
+        '@tabletS': { backgroundImage: 'url(' + DANCE_TABLET_JPG + ')' },
+        '@mobileM': { backgroundImage: 'url(' + DANCE_MOBILE_JPG + ')' },
         gridColumn: '1 / span 6',
         gridRow: 'span 2'
       }
     },
+
     {
       props: {
         gridRow: '5',
         gridColumn: '1 / span 2',
         backgroundImage: 'url(' + MUSEUM_JPG + ')'
       }
-
     },
+
     {
       props: {
         gridRow: '5',
         gridColumn: '3 / span 4',
-        backgroundImage: 'url(' + STALIN_JPG + ')'
-
+        backgroundImage: 'url(' + STALIN_JPG + ')',
+        '@tabletS': { backgroundImage: 'url(' + STALIN_TABLET_JPG + ')' },
+        '@mobileM': { backgroundImage: 'url(' + STALIN_MOBILE_JPG + ')' }
       }
     }
   ]
