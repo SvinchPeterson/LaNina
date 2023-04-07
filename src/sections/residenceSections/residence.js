ddf7187678d535fec075eeb35b30685ff7d07440
svinchy <svinchy@gmail.com>
2023-04-07 08:49:26 +0400

    fixed residence

--- src/sections/residenceSections/residence.js
+++ src/sections/residenceSections/residence.js
@@ -17,25 +17,15 @@ const props = {
   align: 'flex-start center',
   position: 'relative',
   boxSizing: 'border-box',
-  '@maxTabletS': {
-    fontSize: `${15 / 16}em`
-  },
-  '@maxMobileL': {
-    fontSize: `${14 / 16}em`
-  },
 
   '@maxMobileM': {
-    fontSize: `${12.5 / 16}em`
+    fontSize: `${14 / 16}em`
   },
   '@maxMobileS': {
-    fontSize: `${11.5 / 16}em`
+    fontSize: `${10 / 16}em`
   },
 
-  // '@mobileL': {
-  //   minHeight: '100%',
-  //   padding: '0',
-  //   margin: 'Y2 - 0 -',
-  // },
+  '@maxMobileXS': { fontSize: `${9 / 16}em` },
 
   imageContainer: {
     alignSelf: 'center',
@@ -72,7 +62,7 @@ const props = {
       // boxShadow: 'inset 0px 0px 500px 60px rgba(0, 0, 0, .9)',
       style: { backgroundRepeat: 'no-repeat'},
       '@minTabletL': {style: { backgroundAttachment: 'fixed' } },
-      '@maxTabletL': {style: { backgroundSize: '100% 100%' } },
+      '@maxTabletL': {style: { backgroundSize: 'cover' } },
     }
   },
 
@@ -81,77 +71,22 @@ const props = {
     margin: '-F - - -',
     padding: 'C D1 D B',
     round: '0 F 0 0',
-    // padding: 'C D1 D1 D1',
-    // round: '0 F 0 0',
-    // position: 'absolute',
-    // margin: '- - -H2 -F2',
-    // background: 'cream',
     gap: 'Z',
-    // padding: 'C2 E - D',
     zIndex: '5',
-    // round: '0 F2 0 0',
     boxSizing: 'border-box',
-    // ':after': {
-    //   content: '""',
-    //   position: 'absolute',
-    //   boxSize: 'D 100%',
-    //   zIndex: '10',
-    //   background: 'linear-gradient(to top, rgba(1, 57, 57, 1) 0%, rgba(1, 57, 57, 0) 100%)',
-    //   bottom: '-2px',
-    //   left: '0',
-    //   pointerEvents: 'none',
-    //   style: {
-    //     display: 'none',
-    //     // '@media only screen and (max-width: 768px) and (max-height: 800px)': {
-    //     //   display: 'block'
-    //     // }
-    //   }
-    // },
-    // '@tabletM': { margin: '- - -I -F2' },
-    // '@tabletS': { margin: '- - -I -F' },
-    //  '@mobileL': {
-    //   margin: '0',
-    //   round: '0',
-    //   padding: '0 C',
-    //   background: 'transparent',
-    //   color: 'cream',
-    //   gap: '0',
-    // },
-    // '@mobileS': { padding: '0 B1'},
-
-    // style: {
-    //   '@media only screen and (max-width: 768px) and (max-height: 800px)': {
-    //     overflow: 'hidden',
-    //     maxHeight: `${400 / 16}em`
-    //   },
-    //   '@media only screen and (max-width: 768px) and (max-height: 600px)': {
-    //     maxHeight: `${300 / 16}em`
-    //   }
-    // },
-
     title: {
       fontSize: 'D',
-      // '@mobileL': {
-      //   border: 'solid, cream .7',
-      //   borderWidth: '0 0 1px 0',
-      //   maxWidth: '90%'
-      // }
+      '@maxMobileS': { fontSize: `${20 / 10}em`}
+    },
+    '@maxMobileM': {
+      // padding: 'C C D B',
     },
 
     p: {
       maxWidth: 'H',
       fontSize: `${15 / 16}em`,
-      // '@tabletM': {maxWidth: 'G2'},
-      // '@mobileL': { padding: 'A - A2 -' },
-      // style: {
-      //   '@media only screen and (max-width: 768px) and (max-height: 800px)': {
-      //     maxHeight: `${350 / 16}em`,
-      //     overflowY: 'auto'
-      //   },
-      //   '@media only screen and (max-width: 768px) and (max-height: 600px)': {
-      //     maxHeight: `${250 / 16}em`,
-      //   }
-      // }
+      '@maxMobileM': { maxWidth: 'G3'},
+      '@maxMobileS': { fontSize: `${14 / 10}em`}
     }
   }
 }
