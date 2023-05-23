a019f8915aacb591486e651f940b2375bd063b63
svinchy <svinchy@gmail.com>
2023-05-23 22:11:25 +0400

    ewgerg

--- src/sections/residenceSections/location.js
+++ src/sections/residenceSections/location.js
@@ -10,7 +10,7 @@ const props = {
   width: 'fit-content',
   alignSelf: 'center',
   margin: 'E - E1 -',
-  padding: 'E - - -',
+  padding: 'E - E -',
   position: 'relative',
   '@maxTabletL': { margin: 'C - E1 -'},
   '@maxTabletM': { width: '70%' },
